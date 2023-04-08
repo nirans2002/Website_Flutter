295ad6250802ad4b2d068b763f80c6df65b26f3c
Niran S Narayanan <nirans2002@gmail.com>
2023-04-08 12:41:08 +0530

    elevation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: const Text('Niran'),
       ),
       body: LayoutBuilder(
