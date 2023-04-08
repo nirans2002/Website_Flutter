1d420be6e0516a3c819bfa1faba1a815b19d5981
Niran S Narayanan <nirans2002@gmail.com>
2023-04-08 10:45:36 +0530

    initial commit

---
+++ lib/utils/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFF1872DC),
+      onPrimary: Colors.white,
+      secondary: Colors.black,
+      onSecondary: Colors.white,
+      error: Colors.red,
+      onError: Colors.black,
+      background: Color(0xFFE6E8EB),
+      onBackground: Color(0xFF87909A),
+      surface: Colors.white,
+      onSurface: Colors.black),
+  primaryColor: Colors.white,
+);
