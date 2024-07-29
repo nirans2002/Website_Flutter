44d7eb4af15493986716bcf1266d7e283efa78f3
Niran S Narayanan <nirans2002@gmail.com>
2024-07-29 14:01:47 +0530

    edit test Widget

--- lib/components/test_container.dart
+++ lib/components/test_container.dart
@@ -4,12 +4,7 @@ Widget testContainer(text, color,BuildContext context) {
     return Center(
       child: Container(
         color: color,
-        child: ElevatedButton(
-          child: Text(text),
-          onPressed: () {
-            debugPrint("Press detected from $text");
-          },
-        ),
+        child: Text(text),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,20 @@ import 'package:niransnarayanan/utils/theme.dart';
 import 'pages/home_page/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
- );
- runApp(const MyApp());
+  );
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       // themeMode: ThemeMode.dark,
       theme: darkTheme,
