abbd1c65d0f1dc808bb3812d73eea392dd20e384
Niran S Narayanan <nirans2002@gmail.com>
2024-08-15 17:33:07 +0530

    basic project item

--- lib/components/project_item_tile.dart
+++ lib/components/project_item_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:niransnarayanan/data/project.dart';
 import 'package:niransnarayanan/utils/responsive.dart';
 
@@ -53,6 +54,65 @@ class ProjectItemTile extends StatelessWidget {
                   fontWeight: FontWeight.w100,
                   color: Colors.grey,
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Wrap(
+                  spacing: 8.0,
+                  children: project.tags.map((tag) {
+                    return Chip(
+                      // backgroundColor: Colors.orange,
+                      padding: const EdgeInsets.all(4.0),
+                      side: const BorderSide(color: Colors.grey, width: 0.5),
+                      label: Text(
+                        tag,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w100,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 1.0,
+                  width: double.infinity,
+                  color: Colors.grey,
+                ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    tooltip: "Go to GitHub Repo",
+                    onPressed: () {},
+                    icon: const Icon(
+                      FontAwesomeIcons.github,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  IconButton(
+                    tooltip: "See live project",
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.wb_cloudy_outlined,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  IconButton(
+                    tooltip: "go to documentation",
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.my_library_books_outlined,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
