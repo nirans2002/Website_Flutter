4a30f788fc5cc5ce36989e2f64fdf19a1ef00b2a
Niran S Narayanan <nirans2002@gmail.com>
2024-08-16 16:20:22 +0530

    make props nullable

--- lib/data/project.dart
+++ lib/data/project.dart
@@ -1,22 +1,21 @@
 class Project {
-   Project({
+  Project({
     required this.name,
     required this.description,
-    this.githubRepo = '',
-    this.liveLink = '',
-    this.tags = const [],
-    this.imgUrl = '',
-    this.ytUrl = '',
-    this.documentationUrl = '',
+    required this.tags,
+    this.githubRepo,
+    this.liveLink,
+    this.imgUrl,
+    this.ytUrl,
+    this.documentationUrl,
   });
-
 
   final String name;
   final String description;
-  final String githubRepo;
-  final String liveLink;
+  final String? githubRepo;
+  final String? liveLink;
   final List tags;
-  final String imgUrl;
-  final String ytUrl;
-  final String documentationUrl;
+  final String? imgUrl;
+  final String? ytUrl;
+  final String? documentationUrl;
 }
