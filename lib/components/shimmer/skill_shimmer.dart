d91293dadfd1ad3781bf0103874a0f0b72170d10
Niran S Narayanan <nirans2002@gmail.com>
2024-10-14 22:11:14 +0530

    moving to mobile

---
+++ lib/components/shimmer/skill_shimmer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SkillShimmer extends StatelessWidget {
+  const SkillShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
+
+class projectShimmer extends StatelessWidget {
+  const projectShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
